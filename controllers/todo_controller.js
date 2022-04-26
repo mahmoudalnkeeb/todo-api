0428ae8be3db6f79aef6cd9ee29eef573a3c50a1
mahmoudalnkeeb <ramadanalnkeeb2018@gmail.com>
2022-04-26 20:08:42 +0200

    next()

--- controllers/todo_controller.js
+++ controllers/todo_controller.js
@@ -1,27 +1,30 @@
 const task = require('../models/task');
 
 class Todo {
-  async getAllTasks(req, res) {
+  async getAllTasks(req, res, next) {
     try {
       const todos = await task.find();
       res.status(200).json(todos);
+      next();
     } catch (error) {
       res.status(500).json({ message: 'something went wrong' });
     }
   }
-  async addTask(req, res) {
+  async addTask(req, res, next) {
     try {
       const text = req.body.text;
       const newTask = new task({
         text,
         isOpen: true,
       });
-      task.create(newTask);
+      const taskCreated = task.create(newTask);
+      res.status(200).json(taskCreated);
+      next();
     } catch (error) {
       res.status(500).json({ message: 'something went wrong' });
     }
   }
-  async updateTask(req, res) {
+  async updateTask(req, res, next) {
     try {
       const id = req.body.id;
       const isOpen = req.body.isOpen;
@@ -32,19 +35,21 @@ class Todo {
         }
       );
       res.status(200).json(updatedTask);
+      next();
     } catch (error) {
       res.status(500).json({ message: 'something went wrong' });
     }
   }
-  async deletTask(req, res) {
+  async deletTask(req, res, next) {
     try {
       const id = req.body.id;
       task.findByIdAndDelete({ id });
-      res.status(200).json({message:'task deleted successfully'});
+      res.status(200).json({ message: 'task deleted successfully' });
+      next();
     } catch (error) {
       res.status(500).json({ message: 'something went wrong' });
     }
   }
 }
 
-module.exports = Todo
+module.exports = Todo;
