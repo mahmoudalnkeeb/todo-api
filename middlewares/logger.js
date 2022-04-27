a1d03529e6c04ffe8d4f85d9ce68f8bc6feb42a5
mahmoudalnkeeb <ramadanalnkeeb2018@gmail.com>
2022-04-28 00:13:11 +0200

    update logger

--- middlewares/logger.js
+++ middlewares/logger.js
@@ -1,10 +1,21 @@
-const logger = (req, res, next) => {
-  console.log(`
-    ${req.method}
-     ${req.path}
-     ${JSON.stringify(req.body)}
-     `);
-  next();
+const logger = async (req, res, next) => {
+  const body = JSON.stringify(req.body);
+  if (req.method == 'POST' || req.method == 'PUT') {
+    console.log(
+      `Request: 
+        METHOD: ${req.method}
+        BODY: ${body}
+        PATH: ${req.path} 
+        `
+    );
+    next();
+  } else {
+    console.log(
+      `Request: 
+        METHOD: ${req.method}
+        PATH: ${req.path}`
+    );
+    next();
+  }
 };
-
-module.exports = logger
+module.exports = logger;
